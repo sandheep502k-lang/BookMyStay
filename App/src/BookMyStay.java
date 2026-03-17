598a137e31170923db69671ee968ef46e5ba51b3
sandheep502k-lang <sandheep502k@gmail.com>
2026-03-17 16:19:04 +0530

    Use Case 3: Centralized Room Inventory Management

--- App/src/BookMyStay.java
+++ App/src/BookMyStay.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 abstract class Room {
     protected String type;
     protected int beds;
@@ -9,41 +11,55 @@ abstract class Room {
         this.price = price;
     }
 
+    public String getType() {
+        return type;
+    }
+
     public abstract void display();
 }
 
-// Concrete classes
 class SingleRoom extends Room {
-    public SingleRoom() {
-        super("Single Room", 1, 2000);
-    }
-
-    public void display() {
-        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
-    }
+    public SingleRoom() { super("Single", 1, 2000); }
+    public void display() { System.out.println(type + " | Beds: " + beds + " | ₹" + price); }
 }
 
 class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super("Double Room", 2, 3500);
-    }
-
-    public void display() {
-        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
-    }
+    public DoubleRoom() { super("Double", 2, 3500); }
+    public void display() { System.out.println(type + " | Beds: " + beds + " | ₹" + price); }
 }
 
 class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super("Suite Room", 3, 6000);
+    public SuiteRoom() { super("Suite", 3, 6000); }
+    public void display() { System.out.println(type + " | Beds: " + beds + " | ₹" + price); }
+}
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
     }
 
-    public void display() {
-        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    public void addRoom(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    public void updateAvailability(String type, int change) {
+        inventory.put(type, getAvailability(type) + change);
+    }
+
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> e : inventory.entrySet()) {
+            System.out.println(e.getKey() + " Available: " + e.getValue());
+        }
     }
 }
 
-// Main class
 public class BookMyStay {
 
     public static void main(String[] args) {
@@ -52,19 +68,18 @@ public class BookMyStay {
         Room r2 = new DoubleRoom();
         Room r3 = new SuiteRoom();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        RoomInventory inv = new RoomInventory();
 
-        System.out.println("=== Room Details & Availability ===");
+        inv.addRoom(r1.getType(), 5);
+        inv.addRoom(r2.getType(), 3);
+        inv.addRoom(r3.getType(), 2);
 
-        r1.display();
-        System.out.println("Available: " + singleAvailable);
+        System.out.println("=== Inventory ===");
+        inv.displayInventory();
 
-        r2.display();
-        System.out.println("Available: " + doubleAvailable);
+        inv.updateAvailability("Single", -1);
 
-        r3.display();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("=== After Booking ===");
+        inv.displayInventory();
     }
 }
